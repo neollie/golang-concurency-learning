7cf4c9c475ae6fb0b897feed39ec7aedaacf5ac8
neollie <neollie@azet.sk>
2015-11-01 15:40:59 +0000

    Update comments

--- src/github.com/neollie/examples/patterns/Ex4_FunIn/funInSync1.go
+++ src/github.com/neollie/examples/patterns/Ex4_FunIn/funInSync1.go
@@ -6,15 +6,15 @@ import (
 
 // Fun In funs repetedly one output from channel ch1 followed by output from ch2 into the channel ch
 // Execution of producers is coupled (we want from each one output than continue)
-// Thus we BLOCK ch2 ALWAYS until ch1 is not ready (see. funInSyncOnChanNoBlock for enhancement)
-// Reges: (s1 s2)*
+// Thus we ALWAYS BLOCK ch2 until ch1 is not ready (see. funInSyncOnChanNoBlock for enhancement)
+// Regex: (s1 s2)*
 
 func funInSyncOnChan(ch1 , ch2 chan string) (ch chan string) {
 	ch = make(chan string)
 	go func() {
 		for {
-			ch <- <-ch1 //s1
-			ch <- <-ch2 //s2
+			ch <- <-ch1 // sends string s1
+			ch <- <-ch2 //sends string s2
 		}
 	}()
 
